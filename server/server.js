d77f071fde62b3f7754d3aa1b150d2f97dc0427a
Reid Roberts <116128525+reidroberts24@users.noreply.github.com>
2024-01-18 12:46:39 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-
 const cors = require('cors')
 const databaseUri = process.env.DATABASE_URI;
 const jwtSecret = process.env.JWT_SECRET;
 const PORT = process.env.PORT || 8000;
 
+const mealRoutes = require('./routes/meal.routes');
+const userRoutes = require('./routes/user.routes');
+const app = express();
+
 mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
-
-const app = express();
 
 app.use(cors({
     origin: "http://localhost:5173"
@@ -20,7 +21,6 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
-
 app.get('/', (req, res) => {
     res.send('MyMealMetrics Backend is running!');
 });
@@ -28,4 +28,9 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
 console.log(`Server is running on port ${PORT}`);
 });
+
+
+// Use your imported routes
+app.use('/api', mealRoutes);
+app.use('/api', userRoutes);
 
